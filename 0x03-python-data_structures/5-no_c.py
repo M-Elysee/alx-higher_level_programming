19a9355992937a99010f4472634790c2a8725731
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-06-12 18:11:17 +0000

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     string_list = list(my_string)
     for i in string_list:
-        if i == "c" or i == "C":
+        if i == 'c' or i == 'C':
             string_list.pop(string_list.index(i))
-    return ''.join(string_list)
+    return (''.join(string_list))
