8e87c6b71b9bb74265d711e8e73a8ce712cd49bd
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-08-18 19:11:06 +0000

    0x0E. SQL - More queries-debug

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,4 +3,4 @@
 -- +Results must be sorted in ascending order by cities.id
 -- +You can use only one SELECT statement
 SELECT cities.id, cities.name, states.name FROM cities INNER JOIN
-states ON cities.id = states.id ORDER BY cities.id ASC;
+states ON cities.state_id = states.id ORDER BY cities.id;
