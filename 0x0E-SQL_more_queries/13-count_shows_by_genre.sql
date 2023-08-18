a14f790bdfeaf7dbcf6ab864467b8b25746a3de5
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-08-18 19:17:10 +0000

    0x0E. SQL - More queries-debug1

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,4 +9,4 @@ AS 'number_of_shows'
 FROM tv_genres INNER JOIN tv_show_genres
 ON  tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY 'number_of_shows' DESC;
+ORDER BY number_of_shows DESC;
