f4ea6b7a833463441ac20caa19b35ae37157bbb6
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-09-11 20:28:43 +0000

    0x12. JavaScript - Warm up | debug

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,8 @@
 let i = 2;
 let secBig = 0;
 let big = 0;
-while (parseInt(process.argv[i])) {
-  const arg = parseInt(process.argv[i]);
+while (parseInt(process.argv[i], 10)) {
+  let arg = parseInt(process.argv[i]);
   if (arg > big) {
     secBig = big;
     big = arg;
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 /* a script that prints x times “C is fun” */
 
 const x = parseInt(process.argv[2], 10);
-if (!x) {
+if (x !== 0 && !x) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
