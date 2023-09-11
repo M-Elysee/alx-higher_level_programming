f214c144e8ea0cbea219fd1c23e4c2bb371b4d6a
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-09-11 20:56:24 +0000

    0x12. JavaScript - Warm up | debug2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 /* a script that searches the second biggest integer in the list of arguments */
 
-let i = 2;
-let secBig = 0;
-let big = 0;
-while (parseInt(process.argv[i], 10)) {
-  let arg = parseInt(process.argv[i]);
-  if (arg > big) {
-    secBig = big;
-    big = arg;
-  }
-  i++;
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  console.log(process.argv.sort().reverse()[1]);
 }
-console.log(secBig);
