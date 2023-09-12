8f1ed1448a04ae6a91ecfff7db80b96e77075c71
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-09-12 17:18:31 +0000

    0x13. JavaScript - Objects, Scopes and Closures | debug

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,23 +1,19 @@
 #!/usr/bin/node
 // a class Square that defines a square and inherits
-const Rectangle = require('./4-rectangle');
+const Sqr = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends Sqr {
   charPrint (c) {
     if (!c) {
       this.print();
-      return;
-    }
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row = row + c;
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row = row + c;
+        }
+        console.log(row);
       }
-      console.log(row);
     }
   }
 };
