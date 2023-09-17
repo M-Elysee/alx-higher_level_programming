0097b383321d6e5b5259112634eee229428305bc
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-09-17 19:47:58 +0000

    0x0F. Python - Object-relational mapping | debug4

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """a script that lists all State objects, and
    corresponding City objects, contained in the
-   database hbtn_0e_101_usa
+   database passed as argument
 """
 if __name__ == "__main__":
 
