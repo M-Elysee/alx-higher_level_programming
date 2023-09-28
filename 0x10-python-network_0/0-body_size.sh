f348ec52759f9410b6bd3c87c94e3bf0a53e7b69
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-09-28 14:12:37 +0000

    0x10. Python - Network #0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# a Bash script that takes and  sends a request to URL, and displays the body size of the response
+curl -sI "$1" | grep -i "Content-length" | cut -d " " -f2
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# a Bash script that takes and sends a GET request to the URL, and displays the body of the response A header variable X-School-User-Id must be sent with the value 98
+curl -sX "$1" -H "X-School-User-Id:98"
